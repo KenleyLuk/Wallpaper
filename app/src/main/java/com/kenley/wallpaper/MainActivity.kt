2d2fa486b8b5790ac1b54e92845ebf987b60da6a
KenleyLuk <kentyhung@gmail.com>
2024-06-08 10:44:46 -0400

    feat: create landing page layout

--- app/src/main/java/com/kenley/wallpaper/MainActivity.kt
+++ app/src/main/java/com/kenley/wallpaper/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.kenley.wallpaper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kenley.wallpaper.ui.theme.WallpaperTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +38,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    LandingScreenLayout()
                 }
             }
         }
@@ -29,15 +46,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier)
+fun LandingScreenLayout() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.welocme), // Replace with your image resource
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Button(
+            onClick = {
+                      Log.d("Button Click","true")
+                      },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .align(Alignment.BottomCenter)
+                .padding(start = 20.dp, bottom = 30.dp, end = 20.dp),
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(Color.Black)
+        ) {
+            Text("Start Explore",
+            style = TextStyle(fontSize = 20.sp),
+                color = Color.White
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    WallpaperTheme {
-        Greeting("Android")
-    }
+fun PreviewFullScreenLayout() {
+    LandingScreenLayout()
 }
